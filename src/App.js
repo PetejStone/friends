ae7a40e586fae7ae5a899c58c60ea77661471d5c
PetejStone <petejstoney@gmail.com>
2019-05-08 12:28:56 -0700

    created axios get call and rendered friends to page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/friends')
+    
+    .then(res => this.setState({friends: res.data}) )
+  }
+
+  render() {
   return (
     <div className="App">
-     
+      {this.state.friends.map(friend => 
+        <div>
+          <h2>{friend.name}</h2>
+          <p>Age: {friend.age}</p>
+          <p>Email: {friend.email}</p>
+        </div>
+        
+        )}
     </div>
   );
+  }
 }
 
 export default App;
